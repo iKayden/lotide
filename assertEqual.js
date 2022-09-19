fb570fa704180e701e20b55f3c21d25f3d4cb7b9
ikayden <ikyden23@gmail.com>
2022-09-19 16:35:42 -0700

    trying to push to GitHub take 2

--- assertEqual.js
+++ assertEqual.js
@@ -5,8 +5,3 @@ const assertEqual = function (actual, expected) {
     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 12);
-assertEqual("cat", "cat");

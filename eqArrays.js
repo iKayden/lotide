e0b94f77198c846e8a7f9adfc7235abcba4f1672
ikayden <ikyden23@gmail.com>
2022-09-20 17:00:55 -0700

    made eqArrays.js function

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arrA, arrB) => {
+  let isGood = true;
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) {
+      isGood = false;
+      break;
+    }
+  }
+  console.log(isGood);
+  return isGood;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

b5125255d04cb9840e883f9cafcbff7643f63d3e
ikayden <ikyden23@gmail.com>
2022-09-21 16:47:31 -0700

    Created two new functions findByKey and letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = (arrA, arrB) => {
+  let isGood = true;
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) {
+      isGood = false;
+    }
+  }
+  return isGood;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  sentence = sentence.split(" ").join("").toLowerCase();
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("Hello").e, [1]);
+assertArraysEqual(letterPositions("Hello").l, [2, 3]);

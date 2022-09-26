b58a4a2c7638865916605fa9157ced7b6153479b
ikayden <ikyden23@gmail.com>
2022-09-26 13:21:02 -0700

    refactored tail function and added testing

--- tail.js
+++ tail.js
@@ -1,21 +1,7 @@
-// Comparising function for two variables
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array
-
 const tail = (arr) => {
   let newArray = [...arr];
   newArray.splice(0, 1);
   return newArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is `"Labs"
+module.exports = tail;
